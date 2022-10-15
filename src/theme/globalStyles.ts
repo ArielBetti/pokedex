abb6ca7acd9a11f8b6e095f95f760558be69d84e
ArielBetti <ariel.betti@gmail.com>
2022-10-15 01:04:32 -0300

    refac: new theme

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -3,10 +3,11 @@ import { createGlobalStyle } from "styled-components";
 export const ResetCss = createGlobalStyle`
   html,
   body {
+    animation: fadeIn 0.7s both;
     color: ${({ theme }) => theme.font.colors.pure};
     background-color: ${({ theme }) => theme.colors.background};
     padding: 0;
-    margin: 0;
+    margin-bottom: ${(props) => props.theme.spacing.sm};
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
@@ -16,5 +17,34 @@ export const ResetCss = createGlobalStyle`
   }
   * {
     box-sizing: border-box;
+    font-family: 'Open Sans', sans-serif;
   }
+
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    background-color: transparent;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar {
+    width: 2px;
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: ${(props) => props.theme.colors.primary};
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(40px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0px);
+    }
+  }
+
 `;
